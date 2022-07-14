71f67c0f1150336bac062208ffc4835843e9f18d
sweetyr928 <39157466+sweetyr928@users.noreply.github.com>
2022-07-14 18:55:05 +0900

    Add files via upload

---
+++ python_typeerror.py
@@ -0,0 +1,21 @@
+# type error code - 1
+
+# x = '100.0'
+# print(x + 5)
+
+#debugging - 1
+
+x = 100.0
+print(x + 5)
+
+# type error code - 2
+
+# x=3
+# y=('4')
+# print(x+y)
+
+#debugging - 2
+
+x='3'
+y=('4')
+print(x+y)
