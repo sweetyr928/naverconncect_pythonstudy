c85e8db0b5a24e7fe12ef71f47b0b21da510e851
sweetyr928 <39157466+sweetyr928@users.noreply.github.com>
2022-07-14 18:43:35 +0900

    Add files via upload

---
+++ python_syntexerror.py
@@ -0,0 +1,21 @@
+# syntax error code - 1
+
+#x=1
+#if x==1
+#  print('hi')
+
+#debugging - 1
+
+x=1
+if x==1:
+  print('hi')
+
+# syntax error code - 2
+
+#x='hello'
+#print(x))
+
+#debugging - 2
+
+x='hello'
+print(x)
